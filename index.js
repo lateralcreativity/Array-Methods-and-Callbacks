6868294c529d4a5212da7486bd659cb2c64cd6d2
lateralcreativity <lateralcreativity@outlook.com>
2020-04-20 21:57:08 -0500

    Finished normal problems

--- index.js
+++ index.js
@@ -81,15 +81,15 @@ Hint: Investigate your data to find "team initials"!
 Hint: use `.reduce` */
 
 function getCountryWins(data, teamInitials) {
-    const initials = data.map(function(data){
-        if(data['Home Team Goals'] > data['Away Team Goals']){
-            return data['Home Team Initials'];
+    const winnerInitials = data.map(function(element){
+        if(element['Home Team Goals'] > element['Away Team Goals']){
+            return element['Home Team Initials'];
         } else {
-            return data['Away Team Initials'];
+            return element['Away Team Initials'];
         }
     })
 
-    return initials.reduce(function(accumulator, index){
+    return winnerInitials.reduce(function(accumulator, index){
         if(index === teamInitials){
             accumulator += 1;
         }
@@ -103,9 +103,7 @@ console.log(getCountryWins(getFinals(fifaData), 'BRA'));
 
 /* Task 8: Write a function called getGoals() that accepts a parameter `data` and returns the team with the most goals score per appearance (average goals for) in the World Cup finals */
 
-function getGoals(/* code here */) {
-
-    /* code here */
+function getGoals() {
 
 };
 
@@ -125,13 +123,20 @@ badDefense();
 
 /* Task 10: Write a function called `getAverageGoals` that accepts a parameter `data` and returns the the average number of home team goals and away team goals scored per match (Hint: use .reduce and do this in 2 steps) */
 
-function getAverageGoals(/* code here */) {
+function getAverageGoals(data) {
+    let homeTotal = data.reduce((accumulator, element) => {
+        return accumulator += element['Home Team Goals'];
+    }, 0);
 
-    /* code here */
+    let awayTotal = data.reduce((accumulator, element) => {
+        return accumulator += element['Away Team Goals'];
+    }, 0);
 
+    return `The home average is ${(homeTotal / data.length)}
+    \nThe away average is ${(awayTotal / data.length)}`;
 };
 
-getAverageGoals();
+console.log(getAverageGoals(fifaData));
 
 
 /// STRETCH 🥅 //
